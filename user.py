bb4610246b27556f6d80b4ac31664c96cfe1743a
HaleyAllen <haleyallen2074@gmail.com>
2021-10-26 11:55:34 -0700

    inital commit

--- user.py
+++ user.py
@@ -8,47 +8,40 @@ class User:
     # adding the deposit method
     def make_deposit(self, amount):	
     	self.account_balance += amount	
+        return self
     # adding the withdrawal method
     def make_withdrawal(self, amount):	
         self.account_balance -= amount	
+        return self
     # display balance
     def display_user_balance(self):	
-        return "User: "+self.name+", Balance: "+self.account_balance
+        return"User: "+self.name+", Balance: "+self.account_balance
     # transfer money
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 
 ellen = User("Ellen Merkyl", "ellen@python.com")
 monty = User("Monty Python", "monty@python.com")
 peter = User("Peter Rabbit", "pter.rabbit@python.com")
-print(ellen.name)	
-print(monty.name)	
-print(peter.name)	
 
-ellen.make_deposit(400)
-ellen.make_deposit(900)
-ellen.make_deposit(100)
-ellen.make_withdrawal(800)
+ellen.make_deposit(400).make_deposit(900).make_deposit(100).make_withdrawal(800)
 
 print(ellen.account_balance)
 
-monty.make_deposit(1400)
-monty.make_deposit(1400)
-monty.make_withdrawal(300)
-monty.make_withdrawal(400)
+monty.make_deposit(1400).make_deposit(1400).make_withdrawal(300).make_withdrawal(400)
 
 print(monty.account_balance)
 
-peter.make_deposit(1400)
-peter.make_withdrawal(300)
-peter.make_withdrawal(400)
-peter.make_withdrawal(400)
+
+peter.make_deposit(1400).make_withdrawal(300).make_withdrawal(400).make_withdrawal(400)
 
 print(peter.account_balance)
 
+
 monty.transfer_money(ellen,500)
 
-print(monty.account_balance)
 print(ellen.account_balance)
+print(monty.account_balance)
