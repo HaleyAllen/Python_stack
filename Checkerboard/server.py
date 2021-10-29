273188a618f0903731ed03a285a8b389328747eb
HaleyAllen <haleyallen2074@gmail.com>
2021-10-29 12:28:45 -0700

    intial commit

--- Checkerboard/server.py
+++ Checkerboard/server.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def checkerboard():
     return render_template("index.html", num1=8, num2=8)
 
+@app.route('/<int:num1>')
+def checkerboardX(num1):
+    return render_template("index.html", num1=num1, num2=8)
+
+@app.route('/<int:num1>')
+def checkerboardXY(num1, num2):
+    return render_template("index.html", num1=num1, num2=num2)
+
 if __name__=="__main__":
     app.run(debug=True)
 
