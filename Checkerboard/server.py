e7ccbe9bb1eb5120face1dea5edb754be4f02c93
HaleyAllen <haleyallen2074@gmail.com>
2021-10-29 12:57:47 -0700

    inital commit

--- Checkerboard/server.py
+++ Checkerboard/server.py
@@ -3,23 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def checkerboard():
-    return render_template("index.html", num1=8, num2=8, color1="cornflowerblue", color2="blueviolet")
+    return render_template("index.html", num1=8, num2=8)
 
 @app.route('/<int:num1>')
 def checkerboardX(num1):
-    return render_template("index.html", num1=num1, num2=8, color1="cornflowerblue", color2="blueviolet")
+    return render_template("index.html", num1=num1, num2=8)
 
 @app.route('/<int:num1>/<int:num2>')
 def checkerboardXY(num1, num2):
-    return render_template("index.html", num1=num1, num2=num2, color1="cornflowerblue", color2="blueviolet")
+    return render_template("index.html", num1=num1, num2=num2)
 
-@app.route('/<int:num1>/<int:num2>/<string:color1>/')
-def checkerboardXYC1(num1, num2, color1):
-    return render_template("index.html", num1=num1, num2=num2, color1=color1, color2="blueviolet")
 
-@app.route('/<int:num1>/<int:num2>/<string:color1>/<string:color2>')
-def checkerboardXYC1C2(num1, num2, color1, color2):
-    return render_template("index.html", num1=num1, num2=num2, color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
