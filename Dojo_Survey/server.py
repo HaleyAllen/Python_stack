ae38eabf7e5c497b5dc6c7523e99e6f647c0fd9b
HaleyAllen <haleyallen2074@gmail.com>
2021-10-31 15:10:39 -0700

    inital commit

--- Dojo_Survey/server.py
+++ Dojo_Survey/server.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'keep my secret'
 
 @app.route('/')
 def survey():
     return render_template("index.html")
 
-@app.route('/reset')
-def reset():
+@app.route('/submitted')
+def surveyAnswers():
+
     return render_template("submitted.html")
+
+@app.route('/submitted_process', methods=["POST"])
+def surveyAnswers_process():
+    session['name']=request.form['name']
+    session['location']=request.form['location']
+    session['language']=request.form['language']
+    session['description']=request.form['description']
+    return redirect('/submitted')
 
 if __name__=="__main__":
     app.run(debug=True)
