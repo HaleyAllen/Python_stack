d6283b934cabbaee41bdd7e08d3b10060610f895
HaleyAllen <haleyallen2074@gmail.com>
2021-11-01 00:37:36 -0700

    inital commit

---
+++ insertion_sorting.py
@@ -0,0 +1,13 @@
+def insertion_sort(array):
+    for index in range(1, len(array)):
+        currentValue = array[index]
+        currentPosition = index
+
+        while currentPosition > 0 and array[currentPosition - 1] > currentValue:
+            array[currentPosition] = array[currentPosition -1]
+            currentPosition = currentPosition - 1
+        array[currentPosition] = currentValue
+
+array = [6,5,3,1,8,7,2,4]
+insertion_sort(array)
+print("sorted array: " + str(array))
