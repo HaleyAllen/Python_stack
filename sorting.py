1c5a775923d2fb394761d9db65f35f89517d2fc6
HaleyAllen <haleyallen2074@gmail.com>
2021-11-01 00:27:27 -0700

    inital commit

---
+++ sorting.py
@@ -0,0 +1,13 @@
+def selectionSort(list):
+   for fillslot in range(len(list)-1,0,-1):
+       maxpos=0
+       for location in range(1,fillslot+1):
+           if list[location]>list[maxpos]:
+               maxpos = location
+       temp = nlist[fillslot]
+       list[fillslot] = list[maxpos]
+       list[maxpos] = temp
+
+nlist = [14,46,43,27,57,41,45,21,70]
+selectionSort(nlist)
+print(nlist)
